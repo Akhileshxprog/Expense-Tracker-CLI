f9962b2607eb43f4e405aec9d0a8a2e349779583
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-06-30 15:54:51 +0530

    working on a weekend as usual

--- expense_tracker.py
+++ expense_tracker.py
@@ -2,23 +2,32 @@ import csv
 from datetime import datetime
 import argparse
 
-def save_expense(expenses):
-        with open("expense.csv", "w", newline="") as file:
-            csv_write = csv.writer(file)
-            csv_write.writerow([])
-            csv_write.writerows(expenses)
+# Saving Expenses in a CSV file
 
+# def save_expense(expenses):
+#     with open("expense.csv", "w", newline="") as file:
+#         fieldname = []
+#         csv_write = csv.writer(file)
+#         csv_write.writerow(["ID", "Date", "Description", "Amount"])
+#         csv_write.writerows(expenses)
 
-def load_expenses():
-    try:
-        with open("expense.csv", "r", newline="") as file:
-            csv_read = csv.reader(file)
-            for row in csv_read:
-                load = (", ".join(row))
-            return load
-    except (FileNotFoundError):
-        return []
+# Loading Expenses if expenses file is empty, returns a nothing
 
+# def load_expenses():
+#     try:
+#         with open("expense.csv", "r", newline="") as file:
+#             csv_read = csv.reader(file)
+#             return [row for row in csv_read]
+#     except FileNotFoundError:
+#         return []
 
+# Main code goes here insdide expense_tracker function
 
 def expense_tracker():
+
+    """Using argparse subparser for CLI"""
+
+    parser = argparse.ArgumentParser(description="Expense Tracker CLI")
+    subparser = parser.add_subparsers(dest="command")
+
+    #
