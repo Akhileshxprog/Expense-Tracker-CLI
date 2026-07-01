0d5aca86322de4cb41a9c3c9377b3417f27f9c6a
Akhilesh <akhileshakhilesh2880@gmail.com>
2026-07-01 15:41:56 +0530

    Added the add functionality and chang csv.reader and writer to dict

--- expense_tracker.py
+++ expense_tracker.py
@@ -4,30 +4,49 @@ import argparse
 
 # Saving Expenses in a CSV file
 
-# def save_expense(expenses):
-#     with open("expense.csv", "w", newline="") as file:
-#         fieldname = []
-#         csv_write = csv.writer(file)
-#         csv_write.writerow(["ID", "Date", "Description", "Amount"])
-#         csv_write.writerows(expenses)
+def save_expense(expenses):
+    with open("expense.csv", "w", newline="") as file:
+        fieldname = ["ID", "Date", "Description", "Amount"]
+        csv_write = csv.DictWriter(file, fieldnames=fieldname)
+        csv_write.writerows(expenses)
 
-# Loading Expenses if expenses file is empty, returns a nothing
+# Loading Expenses if expenses file is empty, returns  nothing
 
-# def load_expenses():
-#     try:
-#         with open("expense.csv", "r", newline="") as file:
-#             csv_read = csv.reader(file)
-#             return [row for row in csv_read]
-#     except FileNotFoundError:
-#         return []
+def load_expenses():
+    try:
+        with open("expense.csv", "r", newline="") as file:
+            csv_read = csv.DictReader(file)
+            return [row for row in csv_read]
+    except FileNotFoundError:
+        return []
 
 # Main code goes here insdide expense_tracker function
 
 def expense_tracker():
 
-    """Using argparse subparser for CLI"""
+    """Using argparse subparser function for CLI"""
 
     parser = argparse.ArgumentParser(description="Expense Tracker CLI")
     subparser = parser.add_subparsers(dest="command")
 
-    #
+    # "add" adding functionallity for adding expense from terminal
+    add_parser = subparser.add_parser("add")
+    add_parser.add_argument("--description")
+    add_parser.add_argument("--amount", type=float)
+
+    args = parser.parse_args()
+
+    expenses = load_expenses()
+
+    if args.command == "add":
+        expense_data = {
+            "ID": 1,
+            "Date": datetime.now().strftime("%d-%m-%y"),
+            "Description": args.description,
+            "Amount": args.amount
+        }
+        expenses.append(expense_data)
+        print("Expense Added")
+        save_expense(expenses)
+
+expense_tracker()
